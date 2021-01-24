e0c2c05a30878fddbb31e1781e5c7fe72b377a58
AlexisGBarrientos <alexisgbarrientos@gmail.com>
2021-01-24 20:44:10 -0300

    Se creó Componente Register para manejar Hooks useState

--- clase02/src/App.js
+++ clase02/src/App.js
@@ -1,11 +1,13 @@
 import Header from "./Components/Header";
 import Counter from "./Components/Counter";
 import { Row, Col, Container } from "react-bootstrap";
+import Register from "./Components/Register/Register";
 function App() {
   return (
     <Container>
       <Header titulo="Hola, soy el titulo con props"/>
       <Counter/>
+      <Register/>
     </Container>
   );
 }
---
+++ clase02/src/Components/Register/Register.js
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import { Row, Col, Button } from 'react-bootstrap';
+
+
+const Register = () => {
+    const initialState = {
+        name: "",
+        email: ""
+    }
+    const [user,setUser] = useState(initialState);
+    const handleInput = (e) => {
+        setUser({
+            ...user,
+            [e.target.name] : [e.target.value]
+        });
+    }
+    const handleReset = () => {
+        setUser(initialState);
+    }
+    return (
+        <>
+            <Row>
+                <Col md={12}>
+                    <h3 className="text-center">Formulario Comunacho</h3>
+                </Col>
+            </Row>
+            <Row className="mt-3">
+                <Col md={6}>
+                    
+                    <input
+                    name="name"
+                    placeholder="Ingrese su nombre..."
+                    onChange={handleInput}
+                    value={user.name}
+                    />
+                </Col>
+                <Col md={6}>
+                    <input
+                    name="email"
+                    placeholder="Ingrese su Mail..."
+                    onChange={handleInput}
+                    value={user.email}
+                    />
+                </Col>
+            </Row>
+            <Row className="my-3">
+                <Col md={12}>
+                    <h4>Nombre: {user.name}</h4>
+                    <h4>Email: {user.email}</h4>
+                </Col>
+            </Row>
+            <Row className="mt-3">
+                <Button
+                variant={"danger"}
+                onClick={handleReset}
+                >Reset</Button>
+            </Row>
+        </>
+    );
+}
+
+export default Register;
