1ab4fb567b0d6af6b4611592a94ee5db20bf3e64
AlexisGBarrientos <50384373+AlexisGBarrientos@users.noreply.github.com>
2021-01-26 00:40:28 -0300

    Se inicio ToDoList y su estructura de folders

---
+++ proyectos/todolist/src/Components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import { Row, Col } from "react-bootstrap";
+
+const Footer = () => {
+    return (
+        <Row className="bg-light">
+            <Col>
+                <h5 className="text-center my-5">Desarrollado por Alexis Barrientos - 2021</h5>
+            </Col>
+        </Row>
+    );
+}
+
+export default Footer
---
+++ proyectos/todolist/src/Components/List/List.js
@@ -0,0 +1,16 @@
+import { Row, Col } from "react-bootstrap";
+
+const List = () => {
+    return (
+        <Row>
+            <Col>
+                <h3>Ingresar Tareas:</h3>
+            </Col>
+            <Col>
+                <h3>Lista de Tareas</h3>
+            </Col>
+        </Row>
+    );
+}
+
+export default List;
