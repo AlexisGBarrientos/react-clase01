6c1d6988601f8f1066e86b51644b703db3417fac
AlexisGBarrientos <50384373+AlexisGBarrientos@users.noreply.github.com>
2021-02-03 02:12:00 -0300

    Agregamos Checkbox a las tareas

--- proyectos/todolist/src/Components/List/Item.js
+++ proyectos/todolist/src/Components/List/Item.js
@@ -1,26 +1,27 @@
-import { Alert, Row, Col, Button } from "react-bootstrap";
+import { Alert, Row, Col, Button, Form } from "react-bootstrap";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
   
 
 
 const Item = ({id, task, deleteActivity}) => { // se pasa a traves de las props "...item" todas las propiedades, por eso destructuro item
+    const toasty = () => toast(task);
     const deleteTask = (e) => {
-        toast(task)
         deleteActivity(id);
     };    
     return (
             <Alert key={id} variant={"info"} >
-                <Row className="justify-content-between">
+                <Row className="align-items-center">
                     <Col md={11}>
-                        <p>{task}</p>
+                        <Form.Check type="checkbox" label={task} onClick={toasty}/>
                     </Col>
-                    <Col md={1}>
-                        <p 
+                    <Col md={1} >
+                        <Button
+                        variant="danger"
                         style={{cursor:"pointer"}} 
                         id={id}
                         onClick={deleteTask}
-                        >x</p>
+                        >x</Button>
                     </Col>
                 </Row>
                 <ToastContainer />
