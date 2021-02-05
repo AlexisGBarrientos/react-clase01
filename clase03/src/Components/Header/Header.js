510eb0f3e2409bd6fef5511ae76f24d1f5a667ec
AlexisGBarrientos <50384373+AlexisGBarrientos@users.noreply.github.com>
2021-02-05 10:22:23 -0300

    Inició clase 3 con conceptos de useEffect y más de useState

---
+++ clase03/src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import { Row, Col} from "react-bootstrap";
+
+const Header = () => {
+    return (
+        <Row>
+            <Col>
+                <h1>Clase 03</h1>
+            </Col>
+        </Row>
+
+    )
+}
+
+export default Header;
