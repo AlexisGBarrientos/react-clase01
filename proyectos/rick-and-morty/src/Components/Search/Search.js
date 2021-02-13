c1efed5413849695b3ffa8c1d90e28fe2edc493c
AlexisGBarrientos <50384373+AlexisGBarrientos@users.noreply.github.com>
2021-02-13 20:55:06 -0300

    Se agrega estructura del Searche

--- proyectos/rick-and-morty/src/Components/Search/Search.js
+++ proyectos/rick-and-morty/src/Components/Search/Search.js
@@ -3,7 +3,7 @@ import { Row, Col, Button, Form } from "react-bootstrap";
 const Search = () => {
     return ( 
         <Row className="justify-content-center my-5">
-            <Col md={6} className="p-3">
+            <Col md={6} className="p-3 , ">
                 <Form>
                     <Form.Group>
                         <Form.Control 
